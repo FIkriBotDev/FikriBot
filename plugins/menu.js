132aed6d527812fe7ed399d5ebc3e830d3ffea89
FIkriBotDev <132673557+FIkriBotDev@users.noreply.github.com>
2023-05-19 17:29:50 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,427 +1,425 @@
-let handler = async m => m.reply(`❂═══❖•ೋ•\n
-╟ꂑ 𝐻𝑎𝑖, FikriBot Disini! 👋\n
-╟👨‍👨‍👧‍👧Web:\n
-╟http://fikrifahrezi.rf.gd/\n
-╚»★★ミ\n
-\n
-📊Versi Bot: 2.0\n
-📶Update: Perbaikan Menu\n
-\n
-⃝▣──「 𝐑𝐏𝐆-𝐀𝐁𝐒𝐄𝐍 」───⬣\n
-│ ○ .daily (Ⓛ)\n
-│ ○ .gethadiah\n
-│ ○ .hadiah\n
-│ ○ .monthly (Ⓛ)\n
-│ ○ .weekly (Ⓛ)\n
-▣────────────⬣\n
-\n
-⃝▣──「 𝐑𝐏𝐆 」───⬣\n
-│ ○ .adventure (Ⓛ)\n
-│ ○ .pull <jumlah> (Ⓛ)\n
-│ ○ .pullall (Ⓛ)\n
-│ ○ .atm <jumlah> (Ⓛ)\n
-│ ○ .atmall (Ⓛ)\n
-│ ○ .berkebon (Ⓛ)\n
-│ ○ .feed [pet type] (Ⓛ)\n
-│ ○ .open <crate> (Ⓛ)\n
-│ ○ .heal (Ⓛ)\n
-│ ○ .inv (Ⓛ)\n
-│ ○ .kolam (Ⓛ)\n
-│ ○ .leaderboard [jumlah user]  (Ⓟ)\n
-│ ○ .membunuh @tag (Ⓛ)\n
-│ ○ .meracik [type] (Ⓛ)\n
-│ ○ .merampok @tag (Ⓛ)\n
-│ ○ .mulung (Ⓛ)\n
-│ ○ .nambang (Ⓛ)\n
-│ ○ .nebang (Ⓛ)\n
-│ ○ .ramuan [pet type] (Ⓛ)\n
-│ ○ .shop <sell|buy> <args> (Ⓛ)\n
-│ ○ .shopfish <sell|buy> <args> (Ⓛ)\n
-│ ○ .transfer (Ⓛ)\n
-▣────────────⬣\n
-\n
-⃝▣──「 𝐆𝐀𝐌𝐄 」───⬣\n
-│ ○ .asahotak (Ⓛ)\n
-│ ○ .caklontong (Ⓛ)\n
-│ ○ .delttt (Ⓛ)\n
-│ ○ .dungeon [nama room]\n
-│ ○ .family100 (Ⓛ)\n
-│ ○ .fightcentaur (Ⓛ)\n
-│ ○ .fightgriffin (Ⓛ)\n
-│ ○ .fightkucing (Ⓛ)\n
-│ ○ .fightkyubi (Ⓛ)\n
-│ ○ .fightnaga (Ⓛ)\n
-│ ○ .fightphonix (Ⓛ)\n
-│ ○ .fighting (Ⓛ)\n
-│ ○ .hunter (Ⓛ)\n
-│ ○ .pancing <type> (Ⓛ)\n
-│ ○ .jackpot\n
-│ ○ .judi <jumlah> (Ⓛ)\n
-│ ○ .lengkapikalimat (Ⓛ)\n
-│ ○ .math <mode> (Ⓛ)\n
-│ ○ .siapakahaku (Ⓛ)\n
-│ ○ .suit (Ⓛ)\n
-│ ○ .suitpvp @tag (Ⓛ)\n
-│ ○ .susunkata (Ⓛ)\n
-│ ○ .tebakanime (Ⓛ)\n
-│ ○ .tebakbendera (Ⓛ)\n
-│ ○ .tebakgambar (Ⓛ)\n
-│ ○ .tebakgame (Ⓛ)\n
-│ ○ .tebakkata (Ⓛ)\n
-│ ○ .tebakkimia (Ⓛ)\n
-│ ○ .tebaklagu (Ⓛ)\n
-│ ○ .tebaklirik (Ⓛ)\n
-│ ○ .tekateki (Ⓛ)\n
-│ ○ .tictactoe [custom room name] (Ⓛ)\n
-│ ○ .ttt [custom room name] (Ⓛ)\n
-▣────────────⬣\n
-\n
-⃝▣──「 𝐄𝐗𝐏, 𝐋𝐈𝐌𝐈𝐓 」───⬣\n
-│ ○ .buy <jumlah limit>\n
-│ ○ .buyall\n
-│ ○ .cupon [@user] (Ⓛ)\n
-│ ○ .dompet [@user]\n
-│ ○ .exp [@user]\n
-│ ○ .levelup\n
-│ ○ .limit [@user]\n
-▣────────────⬣\n
-\n
-⃝▣──「 𝐀𝐒𝐔𝐏𝐀𝐍 」───⬣\n
-│ ○ .asupanj (Ⓛ)\n
-│ ○ .asupan (Ⓛ)\n
-│ ○ .asupansantuy (Ⓛ) (Ⓟ)\n
-│ ○ .bkp (Ⓛ) (Ⓟ)\n
-│ ○ .cecanindo (Ⓛ)\n
-│ ○ .cecanjapan (Ⓛ)\n
-│ ○ .cecanrandom (Ⓛ)\n
-│ ○ .cecansantuy (Ⓛ)\n
-│ ○ .cecanukhty (Ⓛ)\n
-│ ○ .cecanvietnam (Ⓛ)\n
-│ ○ .xnxxsearch <query> (Ⓛ)\n
-│ ○ .xnxxdl link (Ⓛ)\n
-▣────────────⬣\n
-\n
-⃝▣──「 𝐒𝐓𝐈𝐊𝐄𝐑 」───⬣\n
-│ ○ .attp <teks> (Ⓛ)\n
-│ ○ .dadu\n
-│ ○ .emojimix (Ⓛ)\n
-│ ○ .semoji (Ⓛ)\n
-│ ○ .smeme (Ⓛ)\n
-│ ○ .stickercry (Ⓛ)\n
-│ ○ .stickerhug (Ⓛ)\n
-│ ○ .stickerkiss (Ⓛ)\n
-│ ○ .stiker (caption|reply media) (Ⓛ)\n
-│ ○ .stiker <url> (Ⓛ)\n
-│ ○ .stikergif (caption|reply media) (Ⓛ)\n
-│ ○ .stikergif <url> (Ⓛ)\n
-│ ○ .stikertelegram <url> (Ⓛ) (Ⓟ)\n
-│ ○ .triggered (Ⓛ)\n
-│ ○ .ttp <teks>\n
-│ ○ .wanted (Ⓛ)\n
-│ ○ .wm <packname>|<author> (Ⓛ)\n
-▣────────────⬣\n
-\n
-⃝▣──「 𝐌𝐀𝐈𝐍 」───⬣\n
-│ ○ .afk [alasan] (Ⓛ)\n
-│ ○ .ceksn (Ⓛ)\n
-│ ○ .menu\n
-│ ○ .ref\n
-│ ○ .daftar <nama>.<umur>\n
-│ ○ .sewa\n
-│ ○ .unreg <SERIAL NUMBER>\n
-▣────────────⬣\n
-\n
-⃝▣──「 𝐊𝐄𝐑𝐀𝐍𝐆 𝐀𝐉𝐀𝐈𝐁 」───⬣\n
-│ ○ .apakah <teks>? (Ⓛ)\n
-│ ○ .kapan <text>? (Ⓛ)\n
-│ ○ .kapankah <text>? (Ⓛ)\n
-│ ○ .siapa <teks> (Ⓛ)\n
-│ ○ .siapakah <teks> (Ⓛ)\n
-▣────────────⬣\n
-\n
-⃝▣──「 𝐐𝐔𝐎𝐓𝐄𝐒 」───⬣\n
-│ ○ .quotesanime (Ⓛ)\n
-│ ○ .bucin (Ⓛ)\n
-│ ○ .kata <opsi> (Ⓛ)\n
-│ ○ .quotes (Ⓛ)\n
-▣────────────⬣\n
-\n
-⃝▣──「 𝐀𝐃𝐌𝐈𝐍 」───⬣\n
-│ ○ .kick @user (Ⓛ)\n
-▣────────────⬣\n
-\n
-⃝▣──「 𝐆𝐑𝐔𝐏 」───⬣\n
-│ ○ .group open / close\n
-│ ○ .add @user (Ⓛ)\n
-│ ○ .+ @user (Ⓛ)\n
-│ ○ .enable <option>\n
-│ ○ .disable <option>\n
-│ ○ .infogrup\n
-│ ○ .grouplist\n
-│ ○ .grouptime <open/close> <number>\n
-│ ○ .kick @user\n
-│ ○ .gc\n
-│ ○ .group\n
-│ ○ .link\n
-│ ○ .listadmin\n
-│ ○ .demote @user\n
-│ ○ .promote @user\n
-│ ○ .hidetag [teks] (Ⓛ)\n
-│ ○ .readviewonce\n
-│ ○ .revoke\n
-│ ○ .setppgroup\n
-│ ○ .setdeskgroup <text>\n
-│ ○ .tagall <pesan>\n
-│ ○ .tagme\n
-▣────────────⬣\n
-\n
-⃝▣──「 𝐈𝐍𝐓𝐄𝐑𝐍𝐄𝐓 」───⬣\n
-│ ○ .anime <judul> (Ⓛ)\n
-│ ○ .blackpink (Ⓛ)\n
-│ ○ .brainly <soal> (Ⓛ)\n
-│ ○ .bts (Ⓛ)\n
-│ ○ .cecan (Ⓛ)\n
-│ ○ .cogan (Ⓛ)\n
-│ ○ .darkjokes\n
-│ ○ .exo (Ⓛ)\n
-│ ○ .get <url> (Ⓛ)\n
-│ ○ .gimage <search> (Ⓛ) (Ⓟ)\n
-│ ○ .githubstalk (Ⓛ)\n
-│ ○ .google <search> (Ⓛ)\n
-│ ○ .igstalk <username> (Ⓛ)\n
-│ ○ .kbbi <kata> (Ⓛ)\n
-│ ○ .loli (Ⓛ)\n
-│ ○ .lirik <teks> (Ⓛ)\n
-│ ○ .peta <wilayah> (Ⓛ)\n
-│ ○ .neko (Ⓛ)\n
-│ ○ .pinterest <text> (Ⓛ)\n
-│ ○ .playstore (Ⓛ)\n
-│ ○ .ppcouple (Ⓛ)\n
-│ ○ .puisi (Ⓛ)\n
-│ ○ .ssweb (Ⓛ)\n
-│ ○ .translate <lang> <teks> (Ⓛ)\n
-│ ○ .waifu (Ⓛ)\n
-│ ○ .wikipedia <text> (Ⓛ)\n
-▣────────────⬣\n
-\n
-⃝▣──「 𝐀𝐍𝐎𝐍𝐘𝐌𝐎𝐔𝐒 𝐂𝐇𝐀𝐓 」───⬣\n
-│ ○ .start,skip,stop,next\n
-│ ○ .sendkontak\n
-▣────────────⬣\n
-\n
-⃝▣──「 𝐃𝐎𝐖𝐍𝐋𝐎𝐀𝐃𝐄𝐑 」───⬣\n
-│ ○ .fb <url>\n
-│ ○ .ig <url> (Ⓛ)\n
-│ ○ .igstory <username> (Ⓛ)\n
-│ ○ .joox <judul> (Ⓛ)\n
-│ ○ .lirplay (Ⓛ)\n
-│ ○ .lirplay2 (Ⓛ)\n
-│ ○ .mediafire <url> (Ⓛ)\n
-│ ○ .play <search> (Ⓛ)\n
-│ ○ .play2 <search> (Ⓛ)\n
-│ ○ .sfile (Ⓛ)\n
-│ ○ .tiktok <url> (Ⓛ)\n
-│ ○ .twitter <url> (Ⓛ)\n
-│ ○ .xnxxdl link (Ⓛ)\n
-│ ○ .ytmp3 <url> [server: id4, en60, en61, en68] (Ⓛ)\n
-│ ○ .yta <url> [server: id4, en60, en61, en68] (Ⓛ)\n
-│ ○ .zippyshare <url> (Ⓛ) (Ⓟ)\n
-▣────────────⬣\n
-\n
-⃝▣──「 𝐁𝐄𝐑𝐈𝐓𝐀 」───⬣\n
-│ ○ .cnnnews (Ⓛ)\n
-│ ○ .kompasnews (Ⓛ)\n
-│ ○ .liputan6 (Ⓛ)\n
-│ ○ .metronews (Ⓛ)\n
-│ ○ .tribunnews (Ⓛ)\n
-▣────────────⬣\n
-\n
-⃝▣──「 𝐓𝐎𝐎𝐋𝐒 」───⬣\n
-│ ○ .calc <expression> (Ⓛ)\n
-│ ○ .carigrup <pencarian> (Ⓛ)\n
-│ ○ .del (Ⓛ)\n
-│ ○ .delete (Ⓛ)\n
-│ ○ .githubsearch <pencarian> (Ⓛ)\n
-│ ○ .kalkulator <soal> (Ⓛ)\n
+let handler = async m => m.reply(`❂═══❖•ೋ•
+╟ꂑ 𝐻𝑎𝑖, FikriBot Disini! 👋
+╚»★★ミ
+
+📊Versi Bot: 3.0
+📶Update: Perbaikan Menu
+
+⃝▣──「 𝐑𝐏𝐆-𝐀𝐁𝐒𝐄𝐍 」───⬣
+│ ○ .daily (Ⓛ)
+│ ○ .gethadiah
+│ ○ .hadiah
+│ ○ .monthly (Ⓛ)
+│ ○ .weekly (Ⓛ)
+▣────────────⬣
+
+⃝▣──「 𝐑𝐏𝐆 」───⬣
+│ ○ .adventure (Ⓛ)
+│ ○ .pull <jumlah> (Ⓛ)
+│ ○ .pullall (Ⓛ)
+│ ○ .atm <jumlah> (Ⓛ)
+│ ○ .atmall (Ⓛ)
+│ ○ .berkebon (Ⓛ)
+│ ○ .feed [pet type] (Ⓛ)
+│ ○ .open <crate> (Ⓛ)
+│ ○ .heal (Ⓛ)
+│ ○ .inv (Ⓛ)
+│ ○ .kolam (Ⓛ)
+│ ○ .leaderboard [jumlah user]  (Ⓟ)
+│ ○ .membunuh @tag (Ⓛ)
+│ ○ .meracik [type] (Ⓛ)
+│ ○ .merampok @tag (Ⓛ)
+│ ○ .mulung (Ⓛ)
+│ ○ .nambang (Ⓛ)
+│ ○ .nebang (Ⓛ)
+│ ○ .ramuan [pet type] (Ⓛ)
+│ ○ .shop <sell|buy> <args> (Ⓛ)
+│ ○ .shopfish <sell|buy> <args> (Ⓛ)
+│ ○ .transfer (Ⓛ)
+▣────────────⬣
+
+⃝▣──「 𝐆𝐀𝐌𝐄 」───⬣
+│ ○ .asahotak (Ⓛ)
+│ ○ .caklontong (Ⓛ)
+│ ○ .delttt (Ⓛ)
+│ ○ .dungeon [nama room]
+│ ○ .family100 (Ⓛ)
+│ ○ .fightcentaur (Ⓛ)
+│ ○ .fightgriffin (Ⓛ)
+│ ○ .fightkucing (Ⓛ)
+│ ○ .fightkyubi (Ⓛ)
+│ ○ .fightnaga (Ⓛ)
+│ ○ .fightphonix (Ⓛ)
+│ ○ .fighting (Ⓛ)
+│ ○ .hunter (Ⓛ)
+│ ○ .pancing <type> (Ⓛ)
+│ ○ .jackpot
+│ ○ .judi <jumlah> (Ⓛ)
+│ ○ .lengkapikalimat (Ⓛ)
+│ ○ .math <mode> (Ⓛ)
+│ ○ .siapakahaku (Ⓛ)
+│ ○ .suit (Ⓛ)
+│ ○ .suitpvp @tag (Ⓛ)
+│ ○ .susunkata (Ⓛ)
+│ ○ .tebakanime (Ⓛ)
+│ ○ .tebakbendera (Ⓛ)
+│ ○ .tebakgambar (Ⓛ)
+│ ○ .tebakgame (Ⓛ)
+│ ○ .tebakkata (Ⓛ)
+│ ○ .tebakkimia (Ⓛ)
+│ ○ .tebaklagu (Ⓛ)
+│ ○ .tebaklirik (Ⓛ)
+│ ○ .tekateki (Ⓛ)
+│ ○ .tictactoe [custom room name] (Ⓛ)
+│ ○ .ttt [custom room name] (Ⓛ)
+▣────────────⬣
+
+⃝▣──「 𝐄𝐗𝐏, 𝐋𝐈𝐌𝐈𝐓 」───⬣
+│ ○ .buy <jumlah limit>
+│ ○ .buyall
+│ ○ .cupon [@user] (Ⓛ)
+│ ○ .dompet [@user]
+│ ○ .exp [@user]
+│ ○ .levelup
+│ ○ .limit [@user]
+▣────────────⬣
+
+⃝▣──「 𝐀𝐒𝐔𝐏𝐀𝐍 」───⬣
+│ ○ .asupanj (Ⓛ)
+│ ○ .asupan (Ⓛ)
+│ ○ .asupansantuy (Ⓛ) (Ⓟ)
+│ ○ .bkp (Ⓛ) (Ⓟ)
+│ ○ .cecanindo (Ⓛ)
+│ ○ .cecanjapan (Ⓛ)
+│ ○ .cecanrandom (Ⓛ)
+│ ○ .cecansantuy (Ⓛ)
+│ ○ .cecanukhty (Ⓛ)
+│ ○ .cecanvietnam (Ⓛ)
+│ ○ .xnxxsearch <query> (Ⓛ)
+│ ○ .xnxxdl link (Ⓛ)
+▣────────────⬣
+
+⃝▣──「 𝐒𝐓𝐈𝐊𝐄𝐑 」───⬣
+│ ○ .attp <teks> (Ⓛ)
+│ ○ .dadu
+│ ○ .emojimix (Ⓛ)
+│ ○ .semoji (Ⓛ)
+│ ○ .smeme (Ⓛ)
+│ ○ .stickercry (Ⓛ)
+│ ○ .stickerhug (Ⓛ)
+│ ○ .stickerkiss (Ⓛ)
+│ ○ .stiker (caption|reply media) (Ⓛ)
+│ ○ .stiker <url> (Ⓛ)
+│ ○ .stikergif (caption|reply media) (Ⓛ)
+│ ○ .stikergif <url> (Ⓛ)
+│ ○ .stikertelegram <url> (Ⓛ) (Ⓟ)
+│ ○ .triggered (Ⓛ)
+│ ○ .ttp <teks>
+│ ○ .wanted (Ⓛ)
+│ ○ .wm <packname>|<author> (Ⓛ)
+▣────────────⬣
+
+⃝▣──「 𝐌𝐀𝐈𝐍 」───⬣
+│ ○ .afk [alasan] (Ⓛ)
+│ ○ .ceksn (Ⓛ)
+│ ○ .menu
+│ ○ .ref
+│ ○ .daftar <nama>.<umur>
+│ ○ .sewa
+│ ○ .unreg <SERIAL NUMBER>
+▣────────────⬣
+
+⃝▣──「 𝐊𝐄𝐑𝐀𝐍𝐆 𝐀𝐉𝐀𝐈𝐁 」───⬣
+│ ○ .apakah <teks>? (Ⓛ)
+│ ○ .kapan <text>? (Ⓛ)
+│ ○ .kapankah <text>? (Ⓛ)
+│ ○ .siapa <teks> (Ⓛ)
+│ ○ .siapakah <teks> (Ⓛ)
+▣────────────⬣
+
+⃝▣──「 𝐐𝐔𝐎𝐓𝐄𝐒 」───⬣
+│ ○ .quotesanime (Ⓛ)
+│ ○ .bucin (Ⓛ)
+│ ○ .kata <opsi> (Ⓛ)
+│ ○ .quotes (Ⓛ)
+▣────────────⬣
+
+⃝▣──「 𝐀𝐃𝐌𝐈𝐍 」───⬣
+│ ○ .kick @user (Ⓛ)
+▣────────────⬣
+
+⃝▣──「 𝐆𝐑𝐔𝐏 」───⬣
+│ ○ .group open / close
+│ ○ .add @user (Ⓛ)
+│ ○ .+ @user (Ⓛ)
+│ ○ .enable <option>
+│ ○ .disable <option>
+│ ○ .infogrup
+│ ○ .grouplist
+│ ○ .grouptime <open/close> <number>
+│ ○ .kick @user
+│ ○ .gc
+│ ○ .group
+│ ○ .link
+│ ○ .listadmin
+│ ○ .demote @user
+│ ○ .promote @user
+│ ○ .hidetag [teks] (Ⓛ)
+│ ○ .readviewonce
+│ ○ .revoke
+│ ○ .setppgroup
+│ ○ .setdeskgroup <text>
+│ ○ .tagall <pesan>
+│ ○ .tagme
+▣────────────⬣
+
+⃝▣──「 𝐈𝐍𝐓𝐄𝐑𝐍𝐄𝐓 」───⬣
+│ ○ .anime <judul> (Ⓛ)
+│ ○ .blackpink (Ⓛ)
+│ ○ .brainly <soal> (Ⓛ)
+│ ○ .bts (Ⓛ)
+│ ○ .cecan (Ⓛ)
+│ ○ .cogan (Ⓛ)
+│ ○ .darkjokes
+│ ○ .exo (Ⓛ)
+│ ○ .get <url> (Ⓛ)
+│ ○ .gimage <search> (Ⓛ) (Ⓟ)
+│ ○ .githubstalk (Ⓛ)
+│ ○ .google <search> (Ⓛ)
+│ ○ .igstalk <username> (Ⓛ)
+│ ○ .kbbi <kata> (Ⓛ)
+│ ○ .loli (Ⓛ)
+│ ○ .lirik <teks> (Ⓛ)
+│ ○ .peta <wilayah> (Ⓛ)
+│ ○ .neko (Ⓛ)
+│ ○ .pinterest <text> (Ⓛ)
+│ ○ .playstore (Ⓛ)
+│ ○ .ppcouple (Ⓛ)
+│ ○ .puisi (Ⓛ)
+│ ○ .ssweb (Ⓛ)
+│ ○ .translate <lang> <teks> (Ⓛ)
+│ ○ .waifu (Ⓛ)
+│ ○ .wikipedia <text> (Ⓛ)
+▣────────────⬣
+
+⃝▣──「 𝐀𝐍𝐎𝐍𝐘𝐌𝐎𝐔𝐒 𝐂𝐇𝐀𝐓 」───⬣
+│ ○ .start,skip,stop,next
+│ ○ .sendkontak
+▣────────────⬣
+
+⃝▣──「 𝐃𝐎𝐖𝐍𝐋𝐎𝐀𝐃𝐄𝐑 」───⬣
+│ ○ .fb <url>
+│ ○ .ig <url> (Ⓛ)
+│ ○ .igstory <username> (Ⓛ)
+│ ○ .joox <judul> (Ⓛ)
+│ ○ .lirplay (Ⓛ)
+│ ○ .lirplay2 (Ⓛ)
+│ ○ .mediafire <url> (Ⓛ)
+│ ○ .play <search> (Ⓛ)
+│ ○ .play2 <search> (Ⓛ)
+│ ○ .sfile (Ⓛ)
+│ ○ .tiktok <url> (Ⓛ)
+│ ○ .twitter <url> (Ⓛ)
+│ ○ .xnxxdl link (Ⓛ)
+│ ○ .ytmp3 <url> [server: id4, en60, en61, en68] (Ⓛ)
+│ ○ .yta <url> [server: id4, en60, en61, en68] (Ⓛ)
+│ ○ .zippyshare <url> (Ⓛ) (Ⓟ)
+▣────────────⬣
+
+⃝▣──「 𝐁𝐄𝐑𝐈𝐓𝐀 」───⬣
+│ ○ .cnnnews (Ⓛ)
+│ ○ .kompasnews (Ⓛ)
+│ ○ .liputan6 (Ⓛ)
+│ ○ .metronews (Ⓛ)
+│ ○ .tribunnews (Ⓛ)
+▣────────────⬣
+
+⃝▣──「 𝐓𝐎𝐎𝐋𝐒 」───⬣
+│ ○ .calc <expression> (Ⓛ)
+│ ○ .carigrup <pencarian> (Ⓛ)
+│ ○ .del (Ⓛ)
+│ ○ .delete (Ⓛ)
+│ ○ .githubsearch <pencarian> (Ⓛ)
+│ ○ .kalkulator <soal> (Ⓛ)
 │ ○ .mycontact (Ⓛ)
-│ ○ .ocr (Ⓛ)\n
-│ ○ .totext (Ⓛ)\n
-│ ○ .profile [@user]\n
-│ ○ .qr <teks> (Ⓛ)\n
-│ ○ .qrcode <teks> (Ⓛ)\n
-│ ○ .readmore <teks>|<teks> (Ⓛ)\n
-│ ○ .q (Ⓛ)\n
-│ ○ .spamcall <nomor> (Ⓛ) (Ⓟ)\n
-│ ○ .tinyurl <link> (Ⓛ)\n
-│ ○ .totalfitur (Ⓛ)\n
-│ ○ .tourl (Ⓛ)\n
-│ ○ .tts <lang> <teks> (Ⓛ)\n
-│ ○ .upload (caption|reply media) (Ⓛ)\n
-│ ○ .yts <pencarian> (Ⓛ)\n
-│ ○ .ytsearch <pencarian> (Ⓛ)\n
-▣────────────⬣\n
-\n
-⃝▣──「 𝐅𝐔𝐍 」───⬣\n
-│ ○ .alay (Ⓛ)\n
-│ ○ .artinama [name] (Ⓛ)\n
-│ ○ .alay (Ⓛ)\n
-│ ○ .dadu\n
-│ ○ .halah <teks> (Ⓛ)\n
-│ ○ .hilih <teks> (Ⓛ)\n
-│ ○ .huluh <teks> (Ⓛ)\n
-│ ○ .heleh <teks> (Ⓛ)\n
-│ ○ .holoh <teks> (Ⓛ)\n
-│ ○ .jadian (Ⓛ)\n
-│ ○ .meme\n
-│ ○ .namaninja <teks> (Ⓛ)\n
-│ ○ .rate (Ⓛ)\n
-│ ○ .reaction <reply> (Ⓛ)\n
-│ ○ .simi <teks>\n
-│ ○ .simsimi <teks>\n
-│ ○ .simih <teks>\n
-│ ○ .truth (Ⓛ)\n
-▣────────────⬣\n
-\n
-⃝▣──「 𝐃𝐀𝐓𝐀𝐁𝐀𝐒𝐄 」───⬣\n
-▣────────────⬣\n
-\n
-⃝▣──「 𝐕𝐎𝐓𝐈𝐍𝐆 」───⬣\n
-│ ○ .cekvote\n
-│ ○ .hapusvote\n
-│ ○ .mulaivote [alasan] (Ⓛ)\n
-│ ○ .upvote\n
-│ ○ .devote\n
-▣────────────⬣\n
-\n
-⃝▣──「 𝐀𝐁𝐒𝐄𝐍 」───⬣\n
-│ ○ .absen\n
-│ ○ .cekabsen\n
-│ ○ .hapusabsen\n
-│ ○ .mulaiabsen [teks]\n
-▣────────────⬣\n
-\n
-⃝▣──「 𝐂𝐀𝐓𝐀𝐓𝐀𝐍 」───⬣\n
-│ ○ .buatcatatan <title|isi>\n
-│ ○ .hapuscatatan title\n
-│ ○ .lihatcatatan <title>\n
-▣────────────⬣\n
-\n
-⃝▣──「 𝐉𝐀𝐃𝐈𝐀𝐍 」───⬣\n
-│ ○ .cekpacar (Ⓛ)\n
-│ ○ .ikhlasin (Ⓛ)\n
-│ ○ .putus (Ⓛ)\n
-│ ○ .tembak @tag (Ⓛ)\n
-│ ○ .terima @tag (Ⓛ)\n
-│ ○ .tolak @tag (Ⓛ)\n
-▣────────────⬣\n
-\n
-⃝▣──「 𝐈𝐒𝐋𝐀𝐌𝐈 」───⬣\n
-│ ○ .alquran <114> <1>\n
-│ ○ .asmaulhusna [nomor]\n
-│ ○ .hadis\n
-│ ○ .kisahnabi <name> (Ⓛ)\n
-│ ○ .jadwalsalat <kota> (Ⓛ)\n
-▣────────────⬣\n
-\n
-⃝▣──「 𝐎𝐖𝐍𝐄𝐑 」───⬣\n
-│ ○ .addowner [@user]\n
-│ ○ .prem @tag|days\n
-│ ○ .banchat\n
-│ ○ .ban @tag|days\n
-│ ○ .block <@user>\n
-│ ○ .broadcast <teks>\n
-│ ○ .bc <teks>\n
-│ ○ .broadcastchats <teks>\n
-│ ○ .bcchats <teks>\n
-│ ○ .broadcastgroup <teks>\n
-│ ○ .bcgc <teks>\n
-│ ○ .tfcupon\n
-│ ○ .debounce\n
-│ ○ .delowner [@user]\n
-│ ○ .unprem\n
-│ ○ .enable <option>\n
-│ ○ .disable <option>\n
-│ ○ .expired <hari>\n
-│ ○ .getdatabase\n
-│ ○ .getplugin <teks>\n
-│ ○ .getsessi\n
-│ ○ .joins <chat.whatsapp.com> <day>\n
-│ ○ .demote @user\n
-│ ○ .promote @user\n
-│ ○ .resetlimit\n
-│ ○ .reset\n
-│ ○ .savefile <path>\n
-│ ○ .sf <path>\n
-│ ○ .setmenu <teks>\n
-│ ○ .setmenubefore <teks>\n
-│ ○ .setmenuheader <teks>\n
-│ ○ .setmenubody <teks>\n
-│ ○ .setmenufooter <teks>\n
-│ ○ .setmenuafter <teks>\n
-│ ○ .setppbot\n
-│ ○ .simulate <event> [@mention]\n
-│ ○ .otransfer\n
-│ ○ .unbanchat\n
-│ ○ .unban\n
-│ ○ .unblock <@user>\n
-▣────────────⬣\n
-\n
-⃝▣──「 𝗩𝗜𝗥𝗧𝗘𝗫 」───⬣\n
-│ ○ .prank2  (Ⓟ)\n
-│ ○ .prank\n
-│ ○ .prank3\n
-▣────────────⬣\n
-\n
-⃝▣──「 𝐈𝐍𝐅𝐎 」───⬣\n
-│ ○ .allprofile [@user] (Ⓛ)\n
-│ ○ .listbanned\n
-│ ○ .infocovid <country>\n
-│ ○ .owner\n
-│ ○ .creator\n
-│ ○ .infocuaca <city> (Ⓛ)\n
-│ ○ .database (Ⓛ)\n
-│ ○ .user (Ⓛ)\n
-│ ○ .donasi\n
-│ ○ .infogempa (Ⓛ)\n
-│ ○ .listpremium (Ⓛ)\n
-│ ○ .report <fitur> (Ⓛ)\n
-│ ○ .ping\n
-│ ○ .speed\n
-│ ○ .speedtest\n
-▣────────────⬣\n
-\n
-⃝▣──「 𝐀𝐔𝐃𝐈𝐎 」───⬣\n
-│ ○ .tomp3\n
-│ ○ .tovn\n
-│ ○ .bass [vn] (Ⓛ)\n
-│ ○ .blown [vn] (Ⓛ)\n
-│ ○ .deep [vn] (Ⓛ)\n
-│ ○ .earrape [vn] (Ⓛ)\n
-│ ○ .fast [vn] (Ⓛ)\n
-│ ○ .fat [vn] (Ⓛ)\n
-│ ○ .nightcore [vn] (Ⓛ)\n
-│ ○ .reverse [vn] (Ⓛ)\n
-│ ○ .robot [vn] (Ⓛ)\n
-│ ○ .slow [vn] (Ⓛ)\n
-│ ○ .smooth [vn] (Ⓛ)\n
-│ ○ .tupai [vn] (Ⓛ)\n
-│ ○ .vibra [vn] (Ⓛ)\n
-▣────────────⬣\n
-\n
-⃝▣──「 𝐌𝐀𝐊𝐄𝐑 」───⬣\n
-│ ○ .tahta <teks> (Ⓛ)\n
-│ ○ .lolice\n
-│ ○ .togif (reply)\n
-│ ○ .toimg (reply) (Ⓛ)\n
-▣────────────⬣\n
-\n
+│ ○ .ocr (Ⓛ)
+│ ○ .totext (Ⓛ)
+│ ○ .profile [@user]
+│ ○ .qr <teks> (Ⓛ)
+│ ○ .qrcode <teks> (Ⓛ)
+│ ○ .readmore <teks>|<teks> (Ⓛ)
+│ ○ .q (Ⓛ)
+│ ○ .spamcall <nomor> (Ⓛ) (Ⓟ)
+│ ○ .tinyurl <link> (Ⓛ)
+│ ○ .totalfitur (Ⓛ)
+│ ○ .tourl (Ⓛ)
+│ ○ .tts <lang> <teks> (Ⓛ)
+│ ○ .upload (caption|reply media) (Ⓛ)
+│ ○ .yts <pencarian> (Ⓛ)
+│ ○ .ytsearch <pencarian> (Ⓛ)
+▣────────────⬣
+
+⃝▣──「 𝐅𝐔𝐍 」───⬣
+│ ○ .alay (Ⓛ)
+│ ○ .artinama [name] (Ⓛ)
+│ ○ .alay (Ⓛ)
+│ ○ .dadu
+│ ○ .halah <teks> (Ⓛ)
+│ ○ .hilih <teks> (Ⓛ)
+│ ○ .huluh <teks> (Ⓛ)
+│ ○ .heleh <teks> (Ⓛ)
+│ ○ .holoh <teks> (Ⓛ)
+│ ○ .jadian (Ⓛ)
+│ ○ .meme
+│ ○ .namaninja <teks> (Ⓛ)
+│ ○ .rate (Ⓛ)
+│ ○ .reaction <reply> (Ⓛ)
+│ ○ .simi <teks>
+│ ○ .simsimi <teks>
+│ ○ .simih <teks>
+│ ○ .truth (Ⓛ)
+▣────────────⬣
+
+⃝▣──「 𝐃𝐀𝐓𝐀𝐁𝐀𝐒𝐄 」───⬣
+▣────────────⬣
+
+⃝▣──「 𝐕𝐎𝐓𝐈𝐍𝐆 」───⬣
+│ ○ .cekvote
+│ ○ .hapusvote
+│ ○ .mulaivote [alasan] (Ⓛ)
+│ ○ .upvote
+│ ○ .devote
+▣────────────⬣
+
+⃝▣──「 𝐀𝐁𝐒𝐄𝐍 」───⬣
+│ ○ .absen
+│ ○ .cekabsen
+│ ○ .hapusabsen
+│ ○ .mulaiabsen [teks]
+▣────────────⬣
+
+⃝▣──「 𝐂𝐀𝐓𝐀𝐓𝐀𝐍 」───⬣
+│ ○ .buatcatatan <title|isi>
+│ ○ .hapuscatatan title
+│ ○ .lihatcatatan <title>
+▣────────────⬣
+
+⃝▣──「 𝐉𝐀𝐃𝐈𝐀𝐍 」───⬣
+│ ○ .cekpacar (Ⓛ)
+│ ○ .ikhlasin (Ⓛ)
+│ ○ .putus (Ⓛ)
+│ ○ .tembak @tag (Ⓛ)
+│ ○ .terima @tag (Ⓛ)
+│ ○ .tolak @tag (Ⓛ)
+▣────────────⬣
+
+⃝▣──「 𝐈𝐒𝐋𝐀𝐌𝐈 」───⬣
+│ ○ .alquran <114> <1>
+│ ○ .asmaulhusna [nomor]
+│ ○ .hadis
+│ ○ .kisahnabi <name> (Ⓛ)
+│ ○ .jadwalsalat <kota> (Ⓛ)
+▣────────────⬣
+
+⃝▣──「 𝐎𝐖𝐍𝐄𝐑 」───⬣
+│ ○ .addowner [@user]
+│ ○ .prem @tag|days
+│ ○ .banchat
+│ ○ .ban @tag|days
+│ ○ .block <@user>
+│ ○ .broadcast <teks>
+│ ○ .bc <teks>
+│ ○ .broadcastchats <teks>
+│ ○ .bcchats <teks>
+│ ○ .broadcastgroup <teks>
+│ ○ .bcgc <teks>
+│ ○ .tfcupon
+│ ○ .debounce
+│ ○ .delowner [@user]
+│ ○ .unprem
+│ ○ .enable <option>
+│ ○ .disable <option>
+│ ○ .expired <hari>
+│ ○ .getdatabase
+│ ○ .getplugin <teks>
+│ ○ .getsessi
+│ ○ .joins <chat.whatsapp.com> <day>
+│ ○ .demote @user
+│ ○ .promote @user
+│ ○ .resetlimit
+│ ○ .reset
+│ ○ .savefile <path>
+│ ○ .sf <path>
+│ ○ .setmenu <teks>
+│ ○ .setmenubefore <teks>
+│ ○ .setmenuheader <teks>
+│ ○ .setmenubody <teks>
+│ ○ .setmenufooter <teks>
+│ ○ .setmenuafter <teks>
+│ ○ .setppbot
+│ ○ .simulate <event> [@mention]
+│ ○ .otransfer
+│ ○ .unbanchat
+│ ○ .unban
+│ ○ .unblock <@user>
+▣────────────⬣
+
+⃝▣──「 𝗩𝗜𝗥𝗧𝗘𝗫 」───⬣
+│ ○ .prank2  (Ⓟ)
+│ ○ .prank
+│ ○ .prank3
+▣────────────⬣
+
+⃝▣──「 𝐈𝐍𝐅𝐎 」───⬣
+│ ○ .allprofile [@user] (Ⓛ)
+│ ○ .listbanned
+│ ○ .infocovid <country>
+│ ○ .owner
+│ ○ .creator
+│ ○ .infocuaca <city> (Ⓛ)
+│ ○ .database (Ⓛ)
+│ ○ .user (Ⓛ)
+│ ○ .donasi
+│ ○ .infogempa (Ⓛ)
+│ ○ .listpremium (Ⓛ)
+│ ○ .report <fitur> (Ⓛ)
+│ ○ .ping
+│ ○ .speed
+│ ○ .speedtest
+▣────────────⬣
+
+⃝▣──「 𝐀𝐔𝐃𝐈𝐎 」───⬣
+│ ○ .tomp3
+│ ○ .tovn
+│ ○ .bass [vn] (Ⓛ)
+│ ○ .blown [vn] (Ⓛ)
+│ ○ .deep [vn] (Ⓛ)
+│ ○ .earrape [vn] (Ⓛ)
+│ ○ .fast [vn] (Ⓛ)
+│ ○ .fat [vn] (Ⓛ)
+│ ○ .nightcore [vn] (Ⓛ)
+│ ○ .reverse [vn] (Ⓛ)
+│ ○ .robot [vn] (Ⓛ)
+│ ○ .slow [vn] (Ⓛ)
+│ ○ .smooth [vn] (Ⓛ)
+│ ○ .tupai [vn] (Ⓛ)
+│ ○ .vibra [vn] (Ⓛ)
+▣────────────⬣
+
+⃝▣──「 𝐌𝐀𝐊𝐄𝐑 」───⬣
+│ ○ .tahta <teks> (Ⓛ)
+│ ○ .lolice
+│ ○ .togif (reply)
+│ ○ .toimg (reply) (Ⓛ)
+▣────────────⬣
+
 ⃝▣──「 advanced 」───⬣
-│ ○ .listclan\n
-│ ○ .website\n
-▣────────────⬣\n
-\n
-Made by ♡\n
-FikriDev\n
+│ ○ .listclan
+│ ○ .website
+▣────────────⬣
+
+Made by
+FikriDev
 Customizable WhatsApp Bot`.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
 handler.tags = ['info']
